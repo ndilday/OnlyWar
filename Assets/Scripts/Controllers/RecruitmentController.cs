9729124d8edc66c7e539e2ada19901561888a2af
Nathan <nadilday@yahoo.com>
2020-08-27 21:07:22 -0400

    - built out the training logic for scouts - built up the UI for the recruiting report and scout training

--- Assets/Scripts/Controllers/RecruitmentController.cs
+++ Assets/Scripts/Controllers/RecruitmentController.cs
@@ -10,6 +10,16 @@ using Iam.Scripts.Helpers;
 
 namespace Iam.Scripts.Controllers
 {
+    [Flags]
+    public enum TrainingFocuses
+    {
+        None = 0,
+        Physical = 0x1,
+        Vehicles = 0x2,
+        Melee = 0x4,
+        Ranged = 0x8
+    }
+
     public class RecruitmentController : MonoBehaviour
     {
         public UnitTreeView ScoutSquadView;
@@ -17,10 +27,24 @@ namespace Iam.Scripts.Controllers
         public RecruitmentView RecruitmentView;
 
         private Dictionary<int, Unit> _scoutSquads;
+        private Dictionary<int, TrainingFocuses> _squadSkillFocusMap;
+        private int _scoutCount;
+        private int _squadCount;
+        private int _readyCount;
+
+
+        private const string RECRUITER_FORMAT = @"Greetings, sir. My report on the current status of our Neophytes and Aspriants is as follows.
+
+We currently have {0} Neophytes divided amongh {1} squads in our Tenth Company. Of those, I would consider {2} ready to take the sacrament of the Black Carapace and join us as full Battle Brothers.
+
+We have {3} Aspriants in training who have not yet joined the ranks of our Neophytes.
+
+I await any further instructions you have on our recruiting and training efforts.";
 
         public RecruitmentController()
         {
             _scoutSquads = new Dictionary<int, Unit>();
+            _squadSkillFocusMap = new Dictionary<int, TrainingFocuses>();
         }
 
         public void Chapter_Created()
@@ -31,7 +55,8 @@ namespace Iam.Scripts.Controllers
 
         public void EndTurn_OnClick()
         {
-            // at the end of each week, scouts who are on ship or on home planet 
+            // at the end of each week, scouts who are on ship or on home planet get trained and re-evaluated
+            TrainScouts();
             if(GameSettings.Week % 13 == 1)
             {
                 EvaluateScouts();
@@ -44,6 +69,8 @@ namespace Iam.Scripts.Controllers
             PopulateScoutSquadMap();
             ScoutSquadView.gameObject.SetActive(true);
             RecruitmentView.gameObject.SetActive(true);
+            string message = string.Format(RECRUITER_FORMAT, _scoutCount, _squadCount, _readyCount, 0);
+            RecruitmentView.SetRecruiterMessage(message);
             foreach(Unit squad in _scoutSquads.Values)
             {
                 ScoutSquadView.AddLeafUnit(squad.Id, squad.Name);
@@ -57,50 +84,63 @@ namespace Iam.Scripts.Controllers
             foreach(Soldier soldier in squad.Members)
             {
                 SpaceMarine marine = (SpaceMarine)soldier;
-                if(marine.Rank == TempSpaceMarineRanks.ScoutSergeant)
+                if (marine.Rank == TempSpaceMarineRanks.ScoutSergeant)
                 {
                     continue;
                 }
-                if(marine.MeleeScore > 400)
+                squadReport += GetRecruiterDescription(marine);
+            }
+            RecruitmentView.SquadDescription.text = squadReport;
+            RecruitmentView.SetSquadFlags((ushort)_squadSkillFocusMap[unitId]);
+
+        }
+
+        public void OnUnitTraining_Changed(int unitId, ushort newFlags)
+        {
+            _squadSkillFocusMap[unitId] = (TrainingFocuses)newFlags;
+        }
+
+        private string GetRecruiterDescription(SpaceMarine marine)
+        {
+            if (marine.MeleeScore > 400)
+            {
+                if (marine.RangedScore > 60)
                 {
-                    if(marine.RangedScore > 60)
+                    if (marine.MeleeScore > 500 && marine.RangedScore > 70)
                     {
-                        if (marine.MeleeScore > 500 && marine.RangedScore > 70)
-                        {
-                            squadReport += marine.ToString() + " is ready to accept the Black Carapace and become a full Battle Brother in any squad required.\n";
-                        }
-                        else
-                        {
-                            squadReport += marine.ToString() + " could be promoted to Battle Brother if needed, but I would prefer he earn more seasoning first.\n";
-                        }
-                    }
-                    else if(marine.MeleeScore > 500)
-                    {
-                        squadReport += marine.ToString() + " would make an able assault marine, but I would prefer he continue to train until his ranged proficiency meets our standards.\n";
+                        return marine.ToString() + " is ready to accept the Black Carapace and become a full Battle Brother in any squad required.\n";
                     }
                     else
                     {
-                        squadReport += marine.ToString() + " could be promoted to an assault squad in an emergency, but I would not recommend it.\n";
+                        return marine.ToString() + " could be promoted to Battle Brother if needed, but I would prefer he earn more seasoning first.\n";
                     }
                 }
-                else if(marine.RangedScore > 70)
+                else if (marine.MeleeScore > 500)
                 {
-                    squadReport += marine.ToString() + " could be assigned to devastator duties if necessary, but I would prefer he continue to train until his melee proficiency meets our standards.\n";
-                }
-                else if(marine.RangedScore > 60)
-                {
-                    squadReport += marine.ToString() + " could be promoted to a devastator squad in an emergency, but I would not recommend it.\n";
+                    return marine.ToString() + " would make an able assault marine, but I would prefer he continue to train until his ranged proficiency meets our standards.\n";
                 }
                 else
                 {
-                    squadReport += marine.ToString() + " is not ready to become a Battle Brother, and should acquire more seasoning before taking the Black Carapace.\n";
+                    return marine.ToString() + " could be promoted to an assault squad in an emergency, but I would not recommend it.\n";
                 }
             }
-            RecruitmentView.SquadDescription.text = squadReport;
+            else if (marine.RangedScore > 70)
+            {
+                return marine.ToString() + " could be assigned to devastator duties if necessary, but I would prefer he continue to train until his melee proficiency meets our standards.\n";
+            }
+            else if (marine.RangedScore > 60)
+            {
+                return marine.ToString() + " could be promoted to a devastator squad in an emergency, but I would not recommend it.\n";
+            }
+            else
+            {
+                return marine.ToString() + " is not ready to become a Battle Brother, and should acquire more seasoning before taking the Black Carapace.\n";
+            }
         }
-    
+
         private void PopulateScoutSquadMap()
         {
+            Dictionary<int, TrainingFocuses> newMap = new Dictionary<int, TrainingFocuses>();
             foreach (Unit company in GameSettings.Chapter.ChildUnits)
             {
                 foreach (Unit squad in company.ChildUnits)
@@ -108,19 +148,147 @@ namespace Iam.Scripts.Controllers
                     if (squad.UnitTemplate.Name == "Scout Squad")
                     {
                         _scoutSquads[squad.Id] = squad;
+                        if(_squadSkillFocusMap.ContainsKey(squad.Id))
+                        {
+                            newMap[squad.Id] = _squadSkillFocusMap[squad.Id];
+                        }
+                        else
+                        {
+                            newMap[squad.Id] = TrainingFocuses.Physical | TrainingFocuses.Vehicles | TrainingFocuses.Melee | TrainingFocuses.Ranged;
+                        }
+                    }
+                }
+            }
+            _squadSkillFocusMap = newMap;
+        }
+
+        private void TrainScouts()
+        {
+            foreach(Unit squad in _scoutSquads.Values)
+            {
+                bool goodTeacher = false;
+                TrainingFocuses focuses = _squadSkillFocusMap[squad.Id];
+                int numberOfAreas = 0;
+                if ((focuses & TrainingFocuses.Melee) != TrainingFocuses.None) numberOfAreas++;
+                if ((focuses & TrainingFocuses.Physical) != TrainingFocuses.None) numberOfAreas++;
+                if ((focuses & TrainingFocuses.Ranged) != TrainingFocuses.None) numberOfAreas++;
+                if ((focuses & TrainingFocuses.Vehicles) != TrainingFocuses.None) numberOfAreas++;
+                if (numberOfAreas == 0)
+                {
+                    numberOfAreas = 4;
+                    focuses = TrainingFocuses.Melee | TrainingFocuses.Physical | TrainingFocuses.Ranged | TrainingFocuses.Vehicles;
+                }
+                // 200 hours per point means about 5 weeks, so about 1/5 point per week
+                float baseLearning = 0.2f;
+                foreach (Soldier soldier in squad.Members)
+                {
+                    SpaceMarine marine = (SpaceMarine)soldier;
+                    if (marine.Rank == TempSpaceMarineRanks.ScoutSergeant)
+                    {
+                        // he's working as a teacher, give him quarter credit toward teaching
+                        Skill learnedSkill = marine.Skills[TempBaseSkillList.Instance.Teaching.Id];
+                        learnedSkill.AddPoints(0.05f);
+                        if (marine.Intelligence + learnedSkill.SkillBonus >= 12.0f) goodTeacher = true;
+                        break;
+                    }
+                }
+                if (!goodTeacher)
+                {
+                    // with a sub-par teacher, learning is halfway between teaching and practicing
+                    baseLearning *= 0.75f;
+                }
+                foreach (Soldier soldier in squad.Members)
+                {   
+                    SpaceMarine marine = (SpaceMarine)soldier;
+                    if(marine.Rank != TempSpaceMarineRanks.ScoutSergeant)
+                    {
+                        if((focuses & TrainingFocuses.Melee) != TrainingFocuses.None)
+                        {
+                            TrainMelee(marine, baseLearning / numberOfAreas);
+                        }
+                        if ((focuses & TrainingFocuses.Physical) != TrainingFocuses.None)
+                        {
+                            TrainPhysical(marine, baseLearning / numberOfAreas);
+                        }
+                        if ((focuses & TrainingFocuses.Ranged) != TrainingFocuses.None)
+                        {
+                            TrainRanged(marine, baseLearning / numberOfAreas);
+                        }
+                        if ((focuses & TrainingFocuses.Vehicles) != TrainingFocuses.None)
+                        {
+                            TrainVehicles(marine, baseLearning / numberOfAreas);
+                        }
                     }
                 }
             }
         }
 
+        private void TrainMelee(SpaceMarine marine, float points)
+        {
+            float pointShare = points / 5;
+            marine.Skills[TempBaseSkillList.Instance.Sword.Id].AddPoints(pointShare * 2);
+            marine.Skills[TempBaseSkillList.Instance.Shield.Id].AddPoints(pointShare);
+            marine.Skills[TempBaseSkillList.Instance.Axe.Id].AddPoints(pointShare);
+            marine.Skills[TempBaseSkillList.Instance.Fist.Id].AddPoints(pointShare);
+        }
+
+        private void TrainPhysical(SpaceMarine marine, float points)
+        {
+            // Traits are 10 for 11, 20 for 12, 40 for 13, 80 for 14, 160 for 15, 320 for 16
+            // y = (2^(x-11))*10
+            // y = 
+            float pointShare = points / 3;
+            marine.Strength = GetModifiedStat(marine.Strength, pointShare);
+            marine.Dexterity = GetModifiedStat(marine.Dexterity, pointShare);
+            marine.Constitution = GetModifiedStat(marine.Constitution, pointShare);
+        }
+
+        private void TrainRanged(SpaceMarine marine, float points)
+        {
+            float pointShare = points / 5;
+            marine.Skills[TempBaseSkillList.Instance.Bolter.Id].AddPoints(pointShare);
+            marine.Skills[TempBaseSkillList.Instance.Lascannon.Id].AddPoints(pointShare);
+            marine.Skills[TempBaseSkillList.Instance.Flamer.Id].AddPoints(pointShare);
+            marine.Skills[TempBaseSkillList.Instance.Needler.Id].AddPoints(pointShare);
+            marine.Skills[TempBaseSkillList.Instance.Shotgun.Id].AddPoints(pointShare);
+        }
+
+        private void TrainVehicles(SpaceMarine marine, float points)
+        {
+            float pointShare = points / 4;
+            marine.Skills[TempBaseSkillList.Instance.Bike.Id].AddPoints(pointShare);
+            marine.Skills[TempBaseSkillList.Instance.LandSpeeder.Id].AddPoints(pointShare);
+            marine.Skills[TempBaseSkillList.Instance.Rhino.Id].AddPoints(pointShare);
+            marine.Skills[TempBaseSkillList.Instance.GunneryBolter.Id].AddPoints(pointShare);
+        }
+
+        private float GetModifiedStat(float currentValue, float points)
+        {
+            float curPoints = Mathf.Pow(2, currentValue - 11) * 10;
+            return Mathf.Log((curPoints + points) / 10.0f, 2) + 11;
+
+        }
+
         private void EvaluateScouts()
         {
+            _scoutCount = 0;
+            _squadCount = 0;
+            _readyCount = 0;
             foreach(Unit squad in _scoutSquads.Values)
             {
+                _squadCount++;
                 foreach(Soldier soldier in squad.Members)
                 {
                     SpaceMarine marine = (SpaceMarine)soldier;
                     SpaceMarineEvaluator.Instance.EvaluateMarine(marine);
+                    if(marine.Rank == TempSpaceMarineRanks.Scout)
+                    {
+                        _scoutCount++;
+                        if(marine.MeleeScore > 500 && marine.RangedScore > 70)
+                        {
+                            _readyCount++;
+                        }
+                    }
                 }
             }
         }
--- Assets/Scripts/Views/RecruitmentView.cs
+++ Assets/Scripts/Views/RecruitmentView.cs
@@ -6,6 +6,85 @@ namespace Iam.Scripts.Views
 {
     public class RecruitmentView : MonoBehaviour
     {
+        public UnityEvent<int, ushort> OnToggleChange;
         public Text SquadDescription;
+        public Text RecruiterMessage;
+        public GameObject ToggleParent;
+        public Text ToggleHeaderText;
+
+        private int _selectedUnitId;
+        private Toggle[] _toggles;
+        private const string _toggleText = "What would you like this squad to focus on? (Note: selecting all of the options is the same as selecting none of them.)";
+
+        public void Start()
+        {
+            ToggleHeaderText.text = "";
+            ToggleParent.SetActive(false);
+            _toggles = ToggleParent.GetComponentsInChildren<Toggle>();
+
+        }
+
+        public void Unit_Selected(int id)
+        {
+            ToggleHeaderText.text = _toggleText;
+            ToggleParent.SetActive(true);
+            _selectedUnitId = id;
+            Toggle_Clicked();
+        }
+
+        public void SetRecruiterMessage(string message)
+        {
+            RecruiterMessage.text = message;
+        }
+
+        public void SetSquadFlags(ushort flags)
+        {
+            _toggles[0].isOn = (flags & 0x1) == 0x1;
+            _toggles[1].isOn = (flags & 0x2) == 0x2;
+            _toggles[2].isOn = (flags & 0x3) == 0x3;
+            _toggles[3].isOn = (flags & 0x4) == 0x4;
+        }
+
+        public void Toggle_Clicked()
+        {
+            ushort toggled = 0;
+            if (_toggles[0].isOn)
+            {
+                toggled += 1;
+                _toggles[0].GetComponent<Image>().color = Color.grey;
+            }
+            else
+            {
+                _toggles[0].GetComponent<Image>().color = Color.white;
+            }
+            if (_toggles[1].isOn)
+            {
+                toggled += 2;
+                _toggles[1].GetComponent<Image>().color = Color.grey;
+            }
+            else
+            {
+                _toggles[1].GetComponent<Image>().color = Color.white;
+            }
+            if (_toggles[2].isOn)
+            {
+                toggled += 4;
+                _toggles[2].GetComponent<Image>().color = Color.grey;
+            }
+            else
+            {
+                _toggles[2].GetComponent<Image>().color = Color.white;
+            }
+            if (_toggles[3].isOn)
+            {
+                toggled += 8;
+                _toggles[3].GetComponent<Image>().color = Color.grey;
+            }
+            else
+            {
+                _toggles[3].GetComponent<Image>().color = Color.white;
+            }
+            OnToggleChange.Invoke(_selectedUnitId, toggled);
+        }
     }
 }
