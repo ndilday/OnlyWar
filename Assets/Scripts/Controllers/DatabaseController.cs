65fd7f3bde9176f4ea9171f82dad368d0545833a
Nathan <nadilday@yahoo.com>
2020-10-04 15:34:32 -0400

    - adding some new sprites for the main menu - starting to wire up the main menu

--- Assets/Scripts/Controllers/DatabaseController.cs
+++ Assets/Scripts/Controllers/DatabaseController.cs
@@ -6,8 +6,7 @@ using UnityEngine;
 
 public class DatabaseController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void NewGameButton_OnClick()
     {
         string connection = $"URI=file:{Application.streamingAssetsPath}/GameData/OnlyWar.s3db";
         IDbConnection dbcon = new SqliteConnection(connection);
@@ -19,11 +18,5 @@ public class DatabaseController : MonoBehaviour
         {
             Debug.Log($"{reader[0]}, {reader[1]}, {reader[2]}");
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
