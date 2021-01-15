787562fa646807a204986169380272c048e94e43
Nathan <nadilday@yahoo.com>
2021-01-15 16:48:03 -0500

    - adding class diagram - starting some exploratory work on how I want to handle secret planetary factions

---
+++ Assets/Scripts/Models/Planets/PlanetFaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlyWar.Scripts.Models.Planets
+{
+    public class PlanetFaction
+    {
+        public Faction Faction { get; }
+        public bool IsPublic { get; set; }
+        public bool Population { get; set; }
+    }
+}
