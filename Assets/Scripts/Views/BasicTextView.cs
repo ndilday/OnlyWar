963247fcac9f084db3fae87f834643e7b3c959d5
Nathan <nadilday@yahoo.com>
2021-03-23 15:40:34 -0400

    - renamed ArchiveView to BasicTextView, since there really isn't any archive-specific behavior to it right now - added in an editor script to always have play go to the starting screen, so I don't have to manually take that step every time - built a skeleton for the diplomacy dialog

--- Assets/Scripts/Views/BasicTextView.cs
+++ Assets/Scripts/Views/BasicTextView.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace OnlyWar.Scripts.Views
 {
-    class ArchiveView : MonoBehaviour
+    class BasicTextView : MonoBehaviour
     {
         [SerializeField]
         private Text EventReport;
